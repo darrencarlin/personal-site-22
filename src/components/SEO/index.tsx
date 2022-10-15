8a098c20abec687f9014c81c51a83e32836380ed
Darren Carlin <dazftw@gmail.com>
2022-10-15 00:56:01 -0400

    commit

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -2,13 +2,36 @@ import Head from "next/head";
 import React from "react";
 
 const SEO = () => {
+  // get current day of the week
+  const day = new Date().getDay();
+
+  const data = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  // get current weather via free weather api
+  const weather = async () => {
+    const response = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=YOUR_API_KEY"
+    );
+    const data = await response.json();
+    return data;
+  };
+
   return (
     <Head>
-      <title>Home</title>
+      <title>It&apos;s {data[day].toString()}</title>
       <meta
         name="description"
         content="Personal website of web engineer Darren Carlin"
       />
+      <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
   );
 };
