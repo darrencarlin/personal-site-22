27b948da415e7e4e4190475770cfbfa86d2a42fa
Darren Carlin <dazftw@gmail.com>
2023-12-07 22:52:33 -0500

    Adding work

--- src/components/Work/index.tsx
+++ src/components/Work/index.tsx
@@ -3,6 +3,12 @@ import { ListTitle } from "../_shared/style";
 import { Job, Slash, StyledListItem, WorkList } from "./style";
 
 const work = [
+   {
+    date: "2023 - Present",
+    position: "Full Stack Engineer",
+    company: "Fragment Media Group",
+    companyUrl: "https://fragmnt.com/",
+  },
     {
     date: "2023 - Present",
     position: "Senior Front End Engineer",
